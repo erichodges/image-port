34762a8743cad2f9c3649ddbe062753c69290c64
erichodges <14981329+erichodges@users.noreply.github.com>
2019-09-14 14:31:19 -0700

    Updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Eric Hodges`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
