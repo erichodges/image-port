4a326498afdf0b0c434a0840f84ef06572406aee
erichodges <14981329+erichodges@users.noreply.github.com>
2019-09-17 14:29:09 -0700

    Edit: Cleaned up index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Masonry from 'react-masonry-component'
-import Img from 'gatsby-image'
-import Layout from "../components/layout"
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import React from 'react';
+import Masonry from 'react-masonry-component';
+import Layout from "../components/layout";
 
 const IndexPage = ({ data }) => (
   <Layout>
